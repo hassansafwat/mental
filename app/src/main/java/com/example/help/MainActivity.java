41a30a0e83b8f98c6a95b69865fd8d98c67638ed
hassansafwat <hassansafwat727@gmail.com>
2020-04-21 12:20:24 +0200

    first commit

---
+++ app/src/main/java/com/example/help/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.help;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        return true;
+    }
+}
